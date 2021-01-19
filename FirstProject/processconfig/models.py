f5ef15835a5ed7303f8b288b9af0be6df592c560
PETER KONDACS <peter.kondacs2@ing.com>
2021-01-19 22:06:37 +0100

    some changes with James

--- FirstProject/processconfig/models.py
+++ FirstProject/processconfig/models.py
@@ -9,9 +9,18 @@ class ProcessFlows(models.Model):
     order_number = models.IntegerField(default=0)
     sas_program_name = models.CharField(max_length=100, default="")
 
+    @classmethod
+    def create(cls, project_name):
+        item = cls(project_name=project_name)
+        return item
+
     class Meta:
         verbose_name = "Process Flow"
         verbose_name_plural = "Process Flows"
 
     def __str__(self):
         return f"Process Flow {self.process_flow_int} for the project {self.project_name}"
+
+
+item = ProcessFlows.create("Test Entry")
+print(item)
